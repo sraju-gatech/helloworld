9f64a18b327d39e38f8bae609968474232667f94
Saminathan Raju <saminathan.r@gmail.com>
2024-10-21 23:26:26 +0530

    test

---
+++ src/main/java/com/example/controller/StudentController.java
@@ -0,0 +1,28 @@
+package com.example.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.response.StudentResponse;
+
+@RestController
+@RequestMapping("/api/student/")
+public class StudentController {
+
+	@Value("${Student.Name:Saminathan}")
+	private String studName;
+	@Value("${Region:USA}")
+	private String studRegion;
+	@Value("${Age:22}")
+	private String studAge;
+
+	
+	@GetMapping("/get")
+	//@RequestMapping(value = "/get", method = RequestMethod.GET)
+	public String getStudent () {
+		//StudentResponse studentResponse = new StudentResponse(1, "John", "Smith");
+		return studName +" "+studRegion+ " " +studAge; //studentResponse;
+	}
+}
